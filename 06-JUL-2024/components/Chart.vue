a28f5e93252e8af71693e25db452e4702987925a
Yarden <yarden.as@inf.ethz.ch>
2024-07-31 18:22:24 +0200

    Initial

---
+++ 06-JUL-2024/components/Chart.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import VueApexCharts from "vue3-apexcharts";
+
+const options = {
+  chart: {
+    id: "vuechart-example",
+  },
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+  },
+};
+const series = [
+  {
+    name: "series-1",
+    data: [30, 40, 45, 50, 49, 60, 70, 81],
+  },
+];
+</script>
+
+<template>
+  <div>
+    <VueApexCharts
+      width="500"
+      type="bar"
+      :options="options"
+      :series="series"
+    ></VueApexCharts>
+  </div>
+</template>
+
+<style scoped>
+button {
+  background: #26e6a4;
+  border: 0;
+  font-size: 16px;
+  color: "#fff";
+  padding: 10px;
+  margin-left: 28px;
+}
+</style>
---
+++ 06-JUL-2024/components/Reward.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  size: string;
+}>();
+const size = props.size ?? "text-5xl";
+</script>
+
+<template>
+  <div>
+    <fluent-reward-12-filled :class="`text-blue-gray-300 absolute ${size}`" />
+  </div>
+</template>
